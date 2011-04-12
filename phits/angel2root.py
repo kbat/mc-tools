c4dc1653b419c1e621d1fdd217f6621fc92ccd73
batkov <batkov@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2011-04-12 02:45:19 +0000

    angel2root reads 1D histograms

--- phits/angel2root.py
+++ phits/angel2root.py
@@ -5,8 +5,112 @@ from array import array
 from phits import TallyOutputParser
 from ROOT import ROOT, TH1F, TH2F, TFile, TObjArray
 
+"""
+def isData(line):
+    words = line.strip()
+#    print words
+    for w in words:
+        try:
+            float(w)
+        except ValueError:
+            return False
+    return True
+"""
+
+class Angel:
+    fname = None
+    title  = None
+    xtitle = None
+    ytitle = None
+    ztitle = None
+    lines = []
+    histos = TObjArray()
+    ihist = 0 # histogram number
+    fname_out = None
+    def __init__(self, fname):
+        self.fname = fname
+        file = open(fname)
+        self.lines = tuple(file.readlines())
+        file.close()
+
+        self.fname_out = re.sub("\....$", ".root", fname)
+        if fname == self.fname_out:
+            self.fname_out = fname + ".root"
+
+        ipage = -1
+
+        for iline, line in enumerate(self.lines, 0):
+            line.strip()
+            if re.search("newpage:", line):
+                ipage += 1
+                print "page: ", ipage
+            elif re.search("^x:", line):
+                words = line.split()
+                self.xtitle = string.join(words[1:])
+                print self.xtitle
+            elif re.search("^y:", line):
+                words = line.split()
+                self.ytitle = string.join(words[1:])
+                print self.ytitle
+            elif re.search("^z:", line):
+                print "new graph"
+            elif re.search("^h:", line):
+                print "one dimentional graph section"
+                self.Read1DHist(iline)
+            elif re.search("^h[2dc]:", line):
+                print "two dimentional plot section"
+                
+        fout = TFile(self.fname_out, "recreate")
+        self.histos.Write()
+        fout.Close()
+
+
+    def GetNhist(self, line):
+        """
+        Analyzes the section header and return the number of histograms in the section data
+        """
+        words = line.split()
+        if re.search("^n", words[1]) and re.search("^x", words[2]) and re.search("^y", words[3]) and re.search("^n", words[4]):
+            print "Section Header: 1D histo"
+            return 1
+
+    def Read1DHist(self, iline):
+        nhist = self.GetNhist(self.lines[iline])
+        xarray = []
+        xmax = None
+        data = []
+        errors = []
+        for line in self.lines[iline+1:]:
+            line = line.strip()
+            if line == '': break
+            elif re.search("^#", line): continue
+            words = line.split()
+            if nhist == 1: # a single histogram
+                xarray.append(float(words[0]))
+                xmax =        float(words[1])
+                data.append(  float(words[2]))
+                errors.append(float(words[3]))
+        xarray.append(xmax)
+        nbins = len(xarray)-1
+        
+        if nhist == 1:
+            h = TH1F("h%d" % self.ihist, ";%s;%s" % (self.xtitle, self.ytitle), nbins, array('f', xarray))
+            self.ihist += 1
+            for i in range(nbins):
+                val = data[i]
+                err = errors[i] * val
+                h.SetBinContent(i+1, val)
+                h.SetBinError(i+1, err)
+            self.histos.Add(h)
+
+            del xarray[:]
+            del data[:]
+            del errors[:]
 
 def main():
+    """
+    angel2root - ANGEL to ROOT converter
+    """
     verbose = '-v' in sys.argv
     if verbose:
         sys.argv.remove("-v")
@@ -17,7 +121,11 @@ def main():
         fname_out = fname_in + ".root"
     print fname_out
 
-    hists = TObjArray() # array of histograms to be written in the .root file
+    angel = Angel(fname_in)
+
+    sys.exit(0)
+    
+
 
     p = TallyOutputParser(fname_in)
     print "sections: ", p.getSections()
