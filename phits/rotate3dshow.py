49e8f3c5198b46c942d0422146ef8856ef8aeabd
batkov <batkov@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2011-05-02 01:00:29 +0000

    rotate3dshow understands [el]-phi and [el]-the

--- phits/rotate3dshow.py
+++ phits/rotate3dshow.py
@@ -3,43 +3,45 @@
 import re, sys, string
 from os import system, unlink
 
-def getOut(fname_in):
-    """
-    Return the output file name based on the input file name
-    """
-    fname_out = re.sub("\.....?.?", ".avi", fname_in)
-    if fname_out == fname_in:
-        fname_out = fname_in + ".avi"
-    return fname_out
-
 def main():
     """
-    animate-3dshow (theta|phi) nsteps file.phits
-    The .gif file can be created with gifsicle:
-        gifsicle --disposal 2 *.gif > animation.gif
+    rotate3dshow - a script to rotate [t-3dshow] output.
+The PHITS input file must contain a [t-3dshow] section with description of the 1st shot of the animation.
+Then, one of the selected angles will be changed 'nsteps' times, so it will loop full revolution of 360 deg.
+
+Usage: rotate3dshow (e-the|e-phi|l-the|l-phi) nsteps file.phits [output.gif]
+    As it is stated in the PHITS manual, e-the and e-phi rotate the view point angle, while l-the and l-phi correspond to the light source angle.
+    Individual .gif files produced by the program can be viewed with any image viewer. For instance: qiv -m -s -d 0.3 /tmp/rotate3dshow
+    If the file 'output.gif' is specified and a program 'gifsicle' is installed in your system then all the output files will be merged in a single 'output.gif'
     """
 
-    if len(sys.argv) != 4:
-        print "wrong usage", len(sys.argv)
+    allowed_parameters = ('e-the', 'e-phi', 'l-the', 'l-phi')
+
+    if len(sys.argv) != 4 and len(sys.argv) != 5:
+        print 'ERROR: wrong usage.'
+        print main.__doc__
         return 1
 
-    if sys.argv[1] == 'phi':
-        rotatePhi = True
-    elif sys.argv[1] == 'theta':
-        rotatePhi = False
-    else:
-        print 'theta or phi?'
-        print "usage"
+    if sys.argv[1] not in allowed_parameters:
+        print 'ERROR: Wrong parameter: ', sys.argv[1]
+        print '       Allowed are', allowed_parameters
         return 2
 
-    nsteps = int(sys.argv[2])
+    parameter = sys.argv[1] # parameter which we will rotate
+
+    try:
+        nsteps = int(sys.argv[2])
+    except ValueError:
+        print 'ERROR: Parameter "%s" must be an integer.' % sys.argv[2]
+        return 3
 
     fname_in = sys.argv[3] # phits input file
-    fname_out = getOut(fname_in)
-    fname_tmp = "panimate.phits"
+    fname_out = None
+    if len(sys.argv) == 5: fname_out = sys.argv[4]
+    fname_tmp = "/tmp/panimate.phits"
     epsname = "3dshow.eps" # name of the eps file - parameter 'file' in the [t-3dshow] section must be 3dshow.dat
 
-    print fname_in, fname_out
+#    print fname_in, fname_out
 
     file_in = open(fname_in)
     input_data = file_in.readlines()
@@ -52,8 +54,8 @@ def main():
     print "number of steps:", nsteps
     isFirst = True
 
-    system('rm -fr /tmp/animate-3dshow')
-    system('mkdir /tmp/animate-3dshow')
+    system('rm -fr /tmp/rotate3dshow')
+    system('mkdir /tmp/rotate3dshow')
 
     for istep in range(nsteps):
 #        print "step #", istep
@@ -65,24 +67,16 @@ def main():
                     words[2] = str(11) # 3dshow
                     line = string.join(words) + '\n'
 
-                if isFirst: # get the value of initial angle
-                    if rotatePhi and words[0] == 'e-phi':
+                if isFirst:                                     # get the value of initial angle
+                    if words[0] == parameter:
                         angle0 = int(words[2])
                         isFirst = False
-                    elif not rotatePhi and words[0] == 'e-the':
-                        angle0 = int(words[2])
-                        isFirst = False
-                else:
-                    if rotatePhi and words[0] == 'e-phi':
+                else:                                           # rotate
+                    if words[0] == parameter:
                         angle = angle0+istep*angleStep
                         words[2] = str(angle)
                         line = string.join(words) + '\n'
-                        print "phi:", angle
-                    elif not rotatePhi and words[0] == 'e-the':
-                        angle = angle0+istep*angleStep
-                        words[2] = str(angle)
-                        line = string.join(words) + '\n'
-                        print "theta:", angle
+                        print "angle:", angle
                 
             output_data.append(line)
 
@@ -91,10 +85,14 @@ def main():
             tmp_file.write(line)
         tmp_file.close()
         system("phits < %s" % fname_tmp)
-        system("convert -transparent-color white -background white -rotate 90 %s /tmp/animate-3dshow/%.3d.png" % (epsname, istep))
+        system("convert -transparent-color white -background white -rotate 90 %s /tmp/rotate3dshow/%.3d.gif" % (epsname, istep))
 
-#    system('mencoder')
+    print 'the output files are in /tmp/rotate3dshow'
 
+    if fname_out:
+        if fname_out[-4:] == '.gif':
+            print 'generating gif file', fname_out
+            system('which gifsicle >/dev/null && gifsicle --loopcount=forever --disposal 2 /tmp/rotate3dshow/*.gif > %s' % fname_out)
 
     return 0
 
