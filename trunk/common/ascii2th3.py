4d6e6ff4e8d2e68a073d26e3f1e138eeb058438c
batkov <batkov@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2011-10-06 15:48:06 +0000

    ascii2th3 added

--- trunk/common/ascii2th3.py
+++ trunk/common/ascii2th3.py
@@ -32,8 +32,9 @@ def main():
     i = 0 # bin x
     j = 0 # bin y
     k = 0 # bin z
+
+    # first make a list of all coordinates:
     for line in f.readlines():
-#        print nline, "\t", line.strip()
         if re.search("^#", line): continue # skip comments
         w = line.split()
 
@@ -45,50 +46,65 @@ def main():
             x.append(w[1])
             y.append(w[3])
             z.append(w[5])
-        else:
-            if (w[0] != x[i]) and (w[1] != x[i+1]):
-                i = i+1
-                x.append(w[1])
-#            print "check y: ", w[2], y[j]
-            if (w[2] != y[j]) and (w[3] != y[j+1]):
-                j = j+1
-                y.append(w[3])
-            if (w[4] != z[k]) and (w[5] != z[k+1]):
-                k = k+1
-                z.append(w[5])
-
-#        print i, j, k
-
-        val[(i,j,k)] = w[6]
-        relerr[(i,j,k)] = w[7]
+        else: # now we do not care about being uniqe - fix it later
+            x.append(w[1])
+            y.append(w[3])
+            z.append(w[5])
 
         nline = nline+1
 
-    f.close()
-    print "x: ", x
-    print "y: ", y
-    print "z: ", z
-#    print val
-#    print relerr
+    vx = sorted(str2float(list(set(x))))
+    vy = sorted(str2float(list(set(y))))
+    vz = sorted(str2float(list(set(z))))
 
-    # arrays of float
-    vx = str2float(x)
-    vy = str2float(y)
-    vz = str2float(z)
-    # number of bins
-    nx = len(x)-1
-    ny = len(y)-1
-    nz = len(z)-1
+#        print i, j, k
+#        val[(i,j,k)] = w[6]
+#        relerr[(i,j,k)] = w[7]
+
+    print "x: ", vx
+    print "y: ", vy
+    print "z: ", vz
+
+    nx = len(vx)-1
+    ny = len(vy)-1
+    nz = len(vz)-1
+    
+    print nx, ny, nz
 
     h = TH3F("h", "title;x;y", nx, array('f', vx), ny, array('f', vy), nz, array('f', vz))
-    for i in range(nx):
-        for j in range(ny):
-            for k in range(nz):
-                if (i,j,k) in val:
-                    h.SetBinContent(i+1, j+1, k+1, float(val[(i,j,k)]))
-                    h.SetBinError(i+1, j+1, k+1, float(relerr[(i,j,k)])*float(val[(i,j,k)]))
-#                    h.SetBinError(i+1, j+1, k+1, float(relerr[(i,j,k)]))
-    h.Print()
+
+
+    f.seek(0)
+    x0, y0, z0 = 0, 0, 0
+    bin0 = 0
+    for line in f.readlines():
+        if re.search("^#", line): continue # skip comments
+        w = line.split()
+        
+        x0 = (float(w[0]) + float(w[1]))/2.0
+        y0 = (float(w[2]) + float(w[3]))/2.0
+        z0 = (float(w[4]) + float(w[5]))/2.0
+
+        bin0 = h.FindBin(x0, y0, z0)
+        h.SetBinContent(bin0, float(w[6]))
+        h.SetBinError(bin0, float(w[6])*float(w[7]))
+
+    f.close()
+
+#     # arrays of float
+#     vx = str2float(x)
+#     vy = str2float(y)
+#     vz = str2float(z)
+#     # number of bins
+
+#     for i in range(nx):
+#         for j in range(ny):
+#             for k in range(nz):
+#                 if (i,j,k) in val:
+#                     h.SetBinContent(i+1, j+1, k+1, float(val[(i,j,k)]))
+#                     h.SetBinError(i+1, j+1, k+1, float(relerr[(i,j,k)])*float(val[(i,j,k)]))
+# #                    h.SetBinError(i+1, j+1, k+1, float(relerr[(i,j,k)]))
+#     h.Print()
 
     out = TFile("out.root", "recreate")
     h.Write()
