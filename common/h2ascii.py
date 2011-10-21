d9a701c3abbf24bf67d442e57730e894fd33d908
batkov <batkov@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2011-10-21 12:44:20 +0000

    h2ascii added

---
+++ common/h2ascii.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+from sys import argv, exit
+from ROOT import ROOT, TFile, TH1F
+
+
+def main():
+    fname = argv[1]
+    hname = argv[2]
+
+    f = TFile(fname)
+    h = f.Get(hname)
+
+    nbins = h.GetNbinsX()
+    print "#", h.GetTitle()
+    print "#", h.GetXaxis().GetTitle()
+    print "#", h.GetYaxis().GetTitle()
+    for bin in range(1, nbins+1):
+        print h.GetBinLowEdge(bin), h.GetBinLowEdge(bin+1), h.GetBinContent(bin), h.GetBinError(bin)/h.GetBinContent(bin)
+
+
+if __name__ == "__main__":
+    exit(main())
