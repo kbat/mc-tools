734a0eabad41136de4e7bfeba851213234226035
batkov <batkov@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2012-03-01 13:32:32 +0000

    uniform source added

--- trunk/mcnp/mcnp-source.py
+++ trunk/mcnp/mcnp-source.py
@@ -1,4 +1,12 @@
 #! /usr/bin/python -W all
+#
+# Syntaxis:
+### SOURCE 2D PARABOLIC AA BB NX NY ###
+# or
+### SOURCE UNIFORM AA BB NX NY ###
+# AA and BB are full x and y widths
+# NX and NY are numbers of bins along x and y axes
+#
 
 import fileinput
 import sys
@@ -6,13 +14,26 @@ from string import join
 
 def getParabola(fullwidth, x):
     """
-    Return the value of PDF of parabolic distribution:
+    Return the value of PDF of 2D parabolic distribution:
     p(x) = 3/4d * (1-x^2/d^2)
     where d is half-width of parabola.
     Currently returns the non-normalized value (3/4d is ONE)
     """
     d = fullwidth/2.0
     return 1.0-(x*x)/(d*d)
+
+def getUniform(fullwidth, x):
+    """
+    Return the p.d.f of uniform distribution:
+    p(x) = 1/fullwidth  - if |x| <= fullwidth/2
+    p(x) = 0            - otherwise
+    """
+    xmin, xmax = -fullwidth/2.0, fullwidth/2.0
+    if x > xmin and x < xmax:
+        return 1.0/fullwidth
+    else:
+        return 0
+    
 
 def main():
     is_fixed = False
@@ -54,6 +75,46 @@ def main():
                     print "SP2     ", getParabola(BB, ymin+y*dy)
                 else:
                     print " "*8, getParabola(BB, ymin+y*dy)
+        elif line[0:18] == "### SOURCE UNIFORM":
+            print "c %s" % line.rstrip()
+            is_fixed = True
+            words = line.split()
+            if words[-1] != '###':
+                print >> sys.stderr, "ERROR in mcnp-source: wrong source definition format"
+                return 2
+            AA = float(words[3])
+            BB = float(words[4])
+            NX = int(words[5])
+            NY = int(words[6])
+            xmin = -AA/2.0
+            dx = AA/NX
+            ymin = -BB/2.0
+            dy = BB/NY
+            print "c     Full x-width:", AA, "     Number of x-bins:", NX
+            print "c     Full y-width:", BB, "     Number of y-bins:", NY
+            for x in range(NX+1):
+                if x == 0:
+                    print "SI1  A  ", xmin + x*dx
+                else:
+                    print " "*8, xmin + x*dx
+            for x in range(NX+1):
+                if x == 0:
+                    if getUniform(AA, xmin+x*dx) != 0:
+                        print >> sys.stderr, "First bin is not zero"
+                        return 3
+                    print "SP1     ", getUniform(AA, xmin+x*dx)
+                else:
+                    print " "*8, getUniform(AA, xmin+x*dx)
+            for y in range(NY+1):
+                if y == 0:
+                    print "SI2  A  ", ymin + y*dy
+                else:
+                    print " "*8, ymin + y*dy
+            for y in range(NY+1):
+                if y == 0:
+                    print "SP2     ", getUniform(BB, ymin+y*dy)
+                else:
+                    print " "*8, getUniform(BB, ymin+y*dy)
         else:
             print line.rstrip()
     
