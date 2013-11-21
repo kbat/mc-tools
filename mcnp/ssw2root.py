8b1fb498f90cb89e8f2303d8ef0538f37517e109
batkov <batkov@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2013-11-21 16:12:28 +0000

    ssw2root: by default TTree is created with separate branch for each leaf. This is controlled by splitlevel

--- mcnp/ssw2root.py
+++ mcnp/ssw2root.py
@@ -39,28 +39,42 @@ def main():
     fout = TFile(fout_name, "recreate", ssw.getTitle())
     T = TTree("T", ssw.probs)
 #    T.SetMaxTreeSize((Long64_t)1e+18)
-    T.Branch("hits", hits, "history:id:weight:energy:time:x:y:z:wx:wy:k")
+    splitlevel = 1
+    if splitlevel==99:
+        T.Branch("hits", hits, "history:id:weight:energy:time:x:y:z:wx:wy:k")
+    elif splitlevel == 1:
+        T.Branch("history", AddressOf(hits, 'history'), "history")
+        T.Branch("id", AddressOf(hits, 'id'), "id")
+        T.Branch("weight", AddressOf(hits, 'weight'), "weight")
+        T.Branch("energy", AddressOf(hits, 'energy'), "energy")
+        T.Branch("time", AddressOf(hits, 'time'), "time")
+        T.Branch("x", AddressOf(hits, 'x'), "x")
+        T.Branch("y", AddressOf(hits, 'y'), "y")
+        T.Branch("z", AddressOf(hits, 'z'), "z")
+        T.Branch("wx", AddressOf(hits, 'wx'), "wx")
+        T.Branch("wy", AddressOf(hits, 'wy'), "wy")
+        T.Branch("k", AddressOf(hits, 'k'), "k")
 
-#    print "Number of tracks:", ssw.nevt
+
+
     for i in range(ssw.nevt):
         ssb = ssw.readHit()
-#        print >>fout,  " ".join(map(str, ssb))
         hits.history = ssb[0] # >0 = with collision, <0 = without collision
-	hits.id = ssb[1] # surface + particle type + multigroup problem info
-	hits.weight = ssb[2]
-	hits.energy = ssb[3] # [MeV]
-	hits.time = ssb[4] # [shakes]
-	hits.x = ssb[5] # [cm]
-	hits.y = ssb[6] # [cm]
-	hits.z = ssb[7] # [cm]
-	hits.wx = ssb[8]
-	hits.wy = ssb[9]
-	hits.k = ssb[10] # cosine of angle between track and normal to surface jsu (in MCNPX it is called cs)
-	T.Fill()
+        hits.id = ssb[1] # surface + particle type + multigroup problem info
+        hits.weight = ssb[2]
+        hits.energy = ssb[3] # [MeV]
+        hits.time = ssb[4] # [shakes]
+        hits.x = ssb[5] # [cm]
+        hits.y = ssb[6] # [cm]
+        hits.z = ssb[7] # [cm]
+        hits.wx = ssb[8]
+        hits.wy = ssb[9]
+        hits.k = ssb[10] # cosine of angle between track and normal to surface jsu (in MCNPX it is called cs)
+        T.Fill()
 
     T.Print()
     T.SetAlias("theta", "TMath::RadToDeg()*(TMath::ATan2(x,z) > 0 ? TMath::ATan2(x,z) : 2*TMath::Pi()+TMath::ATan2(x,z))");
-    T.SetAlias("i","TMath::Nint(TMath::Abs(id/1E+6))"); # particle type
+    T.SetAlias("i","TMath::Nint(TMath::Abs(id/1E+6))"); # tmp for particle type
     T.SetAlias("JGP","-TMath::Nint(i/200.0)");         # energy group
     T.SetAlias("JC","TMath::Nint(i/100.0) + 2*JGP");   #
     T.SetAlias("IPT","i-100*JC+200*JGP");              # particle type: 1=neutron, 2=photon, 3=electron
