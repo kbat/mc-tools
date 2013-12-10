62087aa781b339785f799e1e15a809e2402ce60c
borghi.nicolo@gmail.com <borghi.nicolo@gmail.com@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2013-12-10 15:09:45 +0000

    - nbmctal.py: 		- Minor changes in the format of the information strings printed 		  when the verbose flag is set to True
    	- nbmctal2root.py:
    		- Conversion to ROOT format implemented in a more general way.
    		  The previous version was only a draft crafted on a specific
    		  5 tally. Now it should be able to work with all the read MCTAL.
    
    		Nicolò Borghi.

--- trunk/mcnp/nbmctal/nbmctal2root.py
+++ trunk/mcnp/nbmctal/nbmctal2root.py
@@ -1,7 +1,8 @@
 import sys
+import time
 from nbmctal import MCTAL
 from array import array
-from ROOT import ROOT, THnSparse, THnSparseF, TFile, TCanvas, TApplication
+from ROOT import ROOT, THnSparse, THnSparseF, TFile
 
 if len(sys.argv) == 3 and sys.argv[2] == "-v":
         verbose = True
@@ -12,7 +13,25 @@ m = MCTAL(sys.argv[1],verbose)
 
 T = m.Read()
 
-rootT = []
+rootFileName = "%s_%s-%s%s" % (sys.argv[1],time.strftime("%Y%m%d"),time.strftime("%H%M%S"),".root")
+
+rootFile = TFile(rootFileName,"RECREATE");
+
+i = 0
+
+usrAxis = []
+usrAxisCount = None
+cosAxis = []
+cosAxisCount = None
+ergAxis = []
+ergAxisCount = None
+timAxis = []
+timAxisCount = None
+
+
+if verbose:
+	print "\n\033[1m[Converting...]\033[0m"
+
 for tally in T:
 	nCells = tally.nCells
 	if tally.nCells == 0: nCells = 1
@@ -31,14 +50,58 @@ for tally in T:
 	nTim = tally.nTim
 	if tally.nTim   == 0: nTim   = 1
 
-	bins = array('i',[nCells,nDir,nUsr,nSeg,nMul,nCos,nErg-1,nTim-1])
-	binsMin = array('d',[0,0,0,0,0,0,0,0])
-	binsMax = array('d',[nCells-1,nDir-1,nUsr-1,nSeg-1,nMul-1,nCos-1,nErg-2,nTim-2])
+	if len(tally.usr) != 0:
+		if tally.usrTC == "t":
+			nUsr = nUsr - 1
+		usrAxisCount = nUsr - 1
+		usrAxis = array('d',tally.usr)
+
+	if tally.segTC == "t":
+		nSeg = nSeg - 1
+
+	if tally.mulTC == "t":
+		nMul = nMul - 1
+
+	if len(tally.cos) != 0:
+		if tally.cosTC == "t":
+			nCos = nCos - 1
+		cosAxisCount = nCos - 1
+		cosAxis = array('d',tally.cos)
+
+	if len(tally.erg) != 0:
+		if tally.ergTC == "t":
+			nErg = nErg - 1
+		ergAxisCount = nErg - 1
+		ergAxis = array('d',tally.erg) 
+
+	if len(tally.tim) != 0:
+		if tally.timTC == "t":
+			nTim = nTim - 1
+		timAxisCount = nTim - 1
+		timAxis = array('d',tally.tim)
+
+	bins    = array('i',[nCells,   nDir,   nUsr,   nSeg,   nMul,   nCos,   nErg,   nTim])
+	binsMin = array('d',[0,        0,      0,      0,      0,      0,      0,      0])
+	binsMax = array('d',[nCells-1, nDir-1, nUsr-1, nSeg-1, nMul-1, nCos-1, nErg-1, nTim-1])
 
 	hs = THnSparseF("Tally_%d" % tally.tallyNumber,"Tally: %5d" % tally.tallyNumber,8,bins,binsMin,binsMax)
 
-	hs.GetAxis(6).Set(nErg-2,array('d',tally.erg))
-	hs.GetAxis(7).Set(nTim-2,array('d',tally.tim))
+	# Axes must be set here and not with the binsMin and binsMax variables because in case of logarithmic binnings
+	# the division would be linear, loosing meaning.
+
+	if len(usrAxis) != 0:
+		hs.GetAxis(2).Set(usrAxisCount,usrAxis)
+
+	if len(cosAxis) != 0:
+		hs.GetAxis(5).Set(cosAxisCount,cosAxis)
+
+        if len(ergAxis) != 0:
+                hs.GetAxis(6).Set(ergAxisCount,ergAxis)
+
+        if len(timAxis) != 0:
+                hs.GetAxis(7).Set(timAxisCount,timAxis)
+
+	# Here we store values
 
 	for f in range(nCells):
 		for d in range(nDir):
@@ -46,11 +109,16 @@ for tally in T:
 				for s in range(nSeg):
 					for m in range(nMul):
 						for c in range(nCos):
-							for e in range(nErg-1):
-								for t in range(nTim-1):
+							for e in range(nErg):
+								for t in range(nTim):
 									hs.SetBinContent(array('i',[f,d,u,s,m,c,e,t]), tally.getValue(f,d,u,s,m,c,e,t,0))
 
 
-	hs.SaveAs(sys.argv[1] + "tally_%d.root" % tally.tallyNumber)
+	hs.Write()
+	if verbose:
+		print " Tally %5d saved" % (tally.tallyNumber)
+	i = i + 1
 
+rootFile.Close()
+print "\n\033[1mROOT file saved to: %s\033[0m\n" % (rootFileName)
 
