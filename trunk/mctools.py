facb9061764dabdacc296668c2203fcd9cceafa3
batkov <batkov@f77f2f04-88a5-fbc9-6f5c-d034816025e4>
2014-06-27 08:17:15 +0000

    Isotope, Material and Compound classes moved to mctools.py

--- trunk/mctools.py
+++ trunk/mctools.py
@@ -1,16 +1,16 @@
 #! /usr/bin/python -W all
 
-from scipy import constants
+# from scipy import constants
 import subprocess
 
-def L2E(l, m=constants.physical_constants['neutron mass'][0]):
+def L2E(l, m=1.674927351e-27): #constants.physical_constants['neutron mass'][0]):
     """
     Angstrom to MeV converter.
     m = particle mass in kg.
     """
     l = l*1.0E-10 # Angstrom -> meter
-    e = constants.physical_constants['atomic unit of charge'][0]
-    h = constants.physical_constants['Planck constant'][0]
+    e = 1.602176565e-19 # constants.physical_constants['atomic unit of charge'][0]
+    h = 6.62606957e-34 # constants.physical_constants['Planck constant'][0]
     p = h/l
     energy = p*p/(2*m)
     return energy/e/1.0E+6
@@ -24,3 +24,123 @@ def GetVariable(f, var):
     pid = p.pid
     output, error = p.communicate()
     return output
+
+
+### MIXTURES ###
+
+class Compound:
+    """ Compound is a mixture of materials with given volume fractions """
+    def __init__(self, name):
+        self.name = name
+        self.materials = []
+        self.vf = [] # volume fractions of corresponding materials
+
+    def AddMaterial(self, m, vf):
+        """ Adds material m with given volume fraction vf """
+        self.materials.append(m)
+        self.vf.append(vf)
+
+    def GetDensity(self):
+        """ Return density of compound """
+        rho = 0.0
+        for j,m in enumerate(self.materials):
+            rho += m.density*self.vf[j]
+        return rho
+
+    def GetMassFraction(self, material):
+        """ Return mass fraction of the given material """
+        mf = None
+        for im, m in enumerate(self.materials):
+            if m == material:
+                mf = m.density*self.vf[im]/self.GetDensity()
+        if mf == None:
+            raise IOError("Compound %s does not contain material %s" % (self.name, material.name))
+        return mf
+        
+
+    def GetAtomicFractions(self):
+        """ Calculates mass fractions """
+        vf = [] # volume fractions of isotopes
+        mf = [] # mass fractions of isotopes
+        af = [] # atomic fractions of isotopes
+        iname = [] # isotope names
+        for im,m in enumerate(self.materials):
+            for ii, i in enumerate(m.isotopes):
+                curvf = m.GetVolumeFraction(i)*self.vf[im] # current volume fraction
+                vf.append(curvf)
+                curmf = self.GetMassFraction(m)*i.A*m.nn[ii]/m.GetA() # current mass fraction
+                mf.append(curmf)
+                af.append(curmf/i.A)
+                iname.append(i.name)
+
+        # normalisation:
+        s = sum(af)
+        for i,v in enumerate(af):
+            af[i] = v/s
+
+        return dict(zip(iname, af))
+
+    def PrintAtomicFractions(self):
+        for i,f in sorted(self.GetAtomicFractions().iteritems()):
+            print i,f
+        print "Density: ", self.GetDensity()
+
+    def Print(self):
+        print "Compound:", self.name
+        print " Density:", self.GetDensity()
+        for j,m in enumerate(self.materials):
+            print "", self.vf[j], "%"
+            m.Print()
+        print " Mass fractions:"
+        self.GetAtomicFractions()
+
+class Material:
+    """ Material is made of isotopes """
+    def __init__(self, name, density):
+        self.name = name
+        self.isotopes = []
+        self.nn = [] # number of corresponding isotopes
+        self.density = density
+
+    def AddIsotope(self, i, n=1):
+        self.isotopes.append(i)
+        self.nn.append(n)
+
+    def GetA(self):
+        """ Return atomic weight """
+        s = 0.0;
+        for j,i in enumerate(self.isotopes):
+            s += self.nn[j]*i.A;
+        return s
+
+    def GetVolumeFraction(self, isotope):
+        """ Return volume fraction of the given isotope """
+        vf = None
+        for j,i in enumerate(self.isotopes):
+            if i == isotope:
+                vf = self.nn[j]/sum(self.nn)
+                pass
+        if vf == None:
+            raise IOError("Material %s does not have isotope %s" % (self.name, isotope.name))
+        return vf
+
+    def Print(self):
+        print " Material:", self.name
+        print "  Density:", self.density
+        print "  Atomic weight: ", self.GetA()
+        print "  Isotopes:"
+        for j,i in enumerate(self.isotopes):
+            print " "*2, self.nn[j],
+            i.Print()
+            print "   Volume fraction in %s: %g" % (self.name, self.GetVolumeFraction(i))
+
+class Isotope:
+    """ Isotopes form material """
+    def __init__(self, name, A):
+        self.name = name
+        self.A = A # atomic weight
+
+    def Print(self):
+        print "\t%s %g" % (self.name, self.A)
+
+### END MIXTURES ###
