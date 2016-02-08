351670d7aa5976d93522fbd6fb2d881aac326788
Konstantin Batkov <konstantin.batkov@esss.se>
2016-02-08 15:45:26 +0100

    ace2root converter added

---
+++ mctools/common/ace2root.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python
+
+#%matplotlib inline
+from pylab import savefig
+import matplotlib.pyplot as plt
+from pyne import ace
+import inspect
+import ROOT, sys, argparse, os
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+def main():
+    """ Converts ACE files into the ROOT format"""
+
+    parser = argparse.ArgumentParser(description=main.__doc__, epilog='Homepage: https://github.com/kbat/mc-tools')
+    parser.add_argument('acefile', type=str, help='input ACE file name')
+    parser.add_argument('rootfile', type=str, default="", nargs='?', help='output ROOT file name')
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.acefile):
+        print >> sys.stderr, "ace2root: File %s does not exist." % args.acefile
+        sys.exit(1)
+
+    if args.rootfile == "":
+        rootFileName = "%s%s" % (args.acefile,".root")
+    else:
+        rootFileName = args.rootfile
+
+    lib = ace.Library(args.acefile)
+    lib.read()
+
+    ntables = len(lib.tables)
+
+    if not ntables:
+        print >> sys.stderr, "ace2root: no tables found in", args.acefile
+        sys.exit(2)
+#    else:
+#        print ntables, "tables found"
+
+    rootfile = ROOT.TFile(rootFileName, "recreate")
+
+    for tname in lib.tables:
+        table = lib.tables[tname]
+#        print tname, table.reactions
+        newdir = ROOT.gDirectory.mkdir(tname, "");
+        newdir.cd()
+        for mt in table.reactions:
+            reaction = table.find_reaction(mt)
+#            print reaction.text
+            gr = ROOT.TGraph(len(table.energy), table.energy, reaction.sigma)
+            gr.SetName("mt%s" % str(mt))
+            gr.SetTitle("%s #bullet MT=%d;Enegy [MeV];#sigma [barn]" % (tname, mt))
+            gr.Write()
+        rootfile.cd()
+    rootfile.Close()
+
+if __name__ == "__main__":
+        sys.exit(main())
