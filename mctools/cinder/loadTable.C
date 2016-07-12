4d6ee3143ad7109ee7eeb94b6817977808284fa8
Nicolò Borghi <borghi.nicolo@gmail.com>
2016-07-12 11:53:36 +0200

    The loadTable.C macro has been written to make the loading process faster.
    	Nicolò Borghi.

---
+++ mctools/cinder/loadTable.C
@@ -0,0 +1,18 @@
+Table *T = new Table();
+
+loadTable() {
+
+	TFile *f = new TFile("alltabs.root"); // Change to the correct file name in case different from the default one
+	TTree *alltabs = (TTree *) f->Get("alltabs");
+
+	TBranch *bT = alltabs->GetBranch("Table");
+	bT->SetAddress(&T);
+	bT->GetEntry(0);
+
+	cout << "Number of nuclides in table: " << T->getNNuclides() << endl;
+	cout << "Number of time steps in table: " << T->getNBeamStates() << endl;
+
+	return 0;
+
+}
+
