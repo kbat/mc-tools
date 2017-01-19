135e8554a11cc6f0a2967d2bbb1ccd633bc6922d
Konstantin Batkov <konstantin.batkov@esss.se>
2017-01-19 16:25:01 +0100

    vol.py works correctly with several cells

--- mctools/mcnp/vol.py
+++ mctools/mcnp/vol.py
@@ -1,16 +1,22 @@
 #! /usr/bin/python
-# generates the vol cards
+# Generates the MCNP vol cards
 
-N = 1509 # total number of cells
-cell = 881
-vol = 2.43904E+05
-print "vol 1 %dr %g 1 %dr" % (cell-2, vol, N-cell-3)
+import sys
+
+# total number of cells:
+N = 3019
 
 vols = {}
-vols[881] = 88.1
-#vols[882] = 88.2
-#vols[885] = 88.5
-print vols
+vols[985] = 2.43687E+5
+vols[1780] = 7.27140E+04
+vols[1776] = 3.69161E+05
+vols[6] = 1.96631E+04
+vols[8] = 1.97065E+04
+vols[10] = 1.95215E+04
+vols[12] = 1.64029E+04
+vols[14] = 1.64034E+04
+vols[16] = 1.65427E+04
+#print vols
 
 
 s = "vol"
@@ -18,14 +24,20 @@ c_prev = 0
 for i,c in enumerate(sorted(vols.iterkeys())):
     v = vols[c]
     dist_to_prev=c-c_prev
-    print "%d:" % i, c,v, dist_to_prev
+    
+    if dist_to_prev==0:
+        print "Error: dist_to_prev is 0"
+        sys.exit(1)
+
+#    print "%d:" % i, c,v, dist_to_prev
+    
     if dist_to_prev==1:
-        s += " %g | " % v
+        s += " %g " % v
     if dist_to_prev==2:
-        s += " 1 %g | " % (v)
+        s += " 1 %g " % (v)
     elif dist_to_prev>2:
-        s += " 1 %dr %g | " % (dist_to_prev-2, v)
+        s += " 1 %dr %g " % (dist_to_prev-2, v)
     c_prev = c
 
-s += " 1 %d r" % (N-c-3)
+s += " 1 %dr" % (N-c-3)
 print s
