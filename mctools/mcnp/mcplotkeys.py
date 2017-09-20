59f554682a54215754d730f371f63acd305a4cdb
Konstantin Batkov <konstantin.batkov@esss.se>
2017-09-20 12:03:11 +0200

    mcplotkeys improved and configured for MCNP6

--- mctools/mcnp/mcplotkeys.py
+++ mctools/mcnp/mcplotkeys.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python -W all
+# Generates xbindkeys configuration file for MCNP viewer
 
 import sys
 
@@ -9,6 +10,9 @@ class XDoTool:
 
     def MouseMove(self, x, y):
         print "mousemove %d %d" % (x, y),
+
+    def MouseMove1(self, coord):
+        return self.MouseMove(coord[0], coord[1])
 
     def Click(self, button, repeat=1):
         if repeat==1:
@@ -25,12 +29,15 @@ class XDoTool:
     def Restore(self):
         print "restore",
 
+    def Redraw(self):
+        self.MouseMove(100,100)
+        self.Click(1)
+
 def main():
-    c_update = [120, 156]
+    COLOR = [25, 650]
     
     update = XDoTool("update (draw) mcplot")
-    update.MouseMove(120, 156)
-    update.Click(1)
+    update.Redraw()
     update.SetKey("Control+Mod4 + r", "m:0x44 + c:27")
 
     q = XDoTool("exit mcplot")
@@ -51,17 +58,17 @@ def main():
     zoom5.SetKey("Control+Mod4 + equal", "m:0x44 + c:21")
 
     xy = XDoTool("show xy projection")
-    xy.MouseMove(25, 712)
+    xy.MouseMove(25, 690)
     xy.Click(1)
     xy.SetKey("Control+Mod4 + x", "m:0x44 + c:53")
 
     yz = XDoTool("show yz projection")
-    yz.MouseMove(183, 714)
+    yz.MouseMove(183, 690)
     yz.Click(1)
     yz.SetKey("Control+Mod4 + y", "m:0x44 + c:29")
 
     zx = XDoTool("show zx projection")
-    zx.MouseMove(336, 714)
+    zx.MouseMove(336, 690)
     zx.Click(1)
     zx.SetKey("Control+Mod4 + z", "m:0x44 + c:52")
 
@@ -79,7 +86,7 @@ def main():
     down.MouseMove(900, 400)
     down.Click(1)
     down.Restore()
-    up.SetKey("Control+Mod4 + Up", "m:0x44 + c:111")
+    down.SetKey("Control+Mod4 + Up", "m:0x44 + c:111")
 
     left  = XDoTool("shift a bit left")
     left.MouseMove(782, 42)
@@ -87,7 +94,7 @@ def main():
     left.MouseMove(850, 450)
     left.Click(1)
     left.Restore()
-    up.SetKey("Control+Mod4 + Left", "m:0x44 + c:113")
+    left.SetKey("Control+Mod4 + Left", "m:0x44 + c:113")
 
     right  = XDoTool("shift a bit right")
     right.MouseMove(782, 42)
@@ -95,34 +102,36 @@ def main():
     right.MouseMove(950, 450)
     right.Click(1)
     right.Restore()
-    up.SetKey("Control+Mod4 + Right", "m:0x44 + c:114")
+    right.SetKey("Control+Mod4 + Right", "m:0x44 + c:114")
 
     T  = XDoTool("color temperature")
     T.MouseMove(1400, 370)
     T.Click(1)
-    T.MouseMove(166, 677)
+    T.MouseMove1(COLOR)
     T.Click(1, 2)
-    T.MouseMove(100, 100)
-    T.Click(1)
-    up.SetKey("Control+Mod4 + t", "m:0x44 + c:28")
+    T.Redraw()
+    T.Restore()
+    T.SetKey("Control+Mod4 + t", "m:0x44 + c:28")
     
     M  = XDoTool("color material")
     M.MouseMove(1400, 335)
     M.Click(1)
-    M.MouseMove(166, 677)
+    M.MouseMove1(COLOR)
     M.Click(1, 2)
-    M.MouseMove(100, 100)
-    M.Click(1)
-    up.SetKey("Control+Mod4 + m", "m:0x44 + c:58")
-
-    
+    M.Redraw()
+    M.SetKey("Control+Mod4 + m", "m:0x44 + c:58")
     
     rotate = XDoTool("rotate")
-    rotate.MouseMove(200, 650)
+    rotate.MouseMove(200, 630)
     rotate.Click(1)
-    rotate.MouseMove(100, 100)
-    rotate.Click(1)
+    M.Redraw()
     rotate.SetKey("Control + Mod4 + e", "m:0x44 + c:26")
+
+    scales = XDoTool("scales")
+    scales.MouseMove(200, 650)
+    scales.Click(1)
+    M.Redraw()
+    scales.SetKey("Control + Mod4 + s", "m:0x44 + c:39")
 
 if __name__ == "__main__":
     sys.exit(main())
