29a05d29a9fbc2e501f05ead5b5ebb2308509ad6
Konstantin Batkov <konstantin.batkov@esss.se>
2018-03-06 16:06:06 +0100

    legend, scale, and mesh keywords added

--- mctools/mcnp/zoom.py
+++ mctools/mcnp/zoom.py
@@ -43,11 +43,20 @@ def main():
                     cmd['label'] = map(int, map(float, words[i+1:i+3])) + [words[i+3]]
                 elif re.search("^p[xyz]", w):
                     cmd[w] = [float(words[i+1])]
+                elif re.search("^legend", w):
+                    cmd[w] = [words[i+1]]
+                elif re.search("^scale", w):
+                    cmd[w] = [words[i+1]]
+                elif re.search("^mesh", w):
+                    cmd[w] = [words[i+1]]
 
-    keys = ('bas', 'or', 'ex', 'px', 'py', 'pz', 'label')
+    keys = ('bas', 'or', 'ex', 'px', 'py', 'pz', 'label', 'legend', 'scale', 'mesh')
     with open(args.comout, 'w') as f:
         for key in keys:
             if key in cmd:
+                # newline needed for mcplot:
+                if key in ('label', 'scale', 'mesh'):
+                    f.write("\n")
                 f.write("%s %s " % (key," ".join(str(e) for e in cmd[key]),))
         f.write("\n")
 
