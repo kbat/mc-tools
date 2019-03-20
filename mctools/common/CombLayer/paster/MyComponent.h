a4fbe158f838e5e9dc0e7765b915310b0fb20deb
Konstantin Batkov <konstantin.batkov@esss.se>
2019-03-20 12:32:26 +0100

    years ranged

--- mctools/common/CombLayer/paster/MyComponent.h
+++ mctools/common/CombLayer/paster/MyComponent.h
@@ -3,7 +3,7 @@
  
  * File:   essBuildInc/MyComponent.h
  *
- * Copyright (c) YEAR by AUTHOR
+ * Copyright (c) YEAR-YEAR by AUTHOR
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
