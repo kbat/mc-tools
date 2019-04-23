c7895efc70e967ae1007ae55cf980f2c1e2c0769
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2019-04-23 16:03:54 +0200

    colon replaced with diamond

--- mctools/fluka/usxsuw2root.py
+++ mctools/fluka/usxsuw2root.py
@@ -77,7 +77,7 @@ def hist(det):
     """ Create histogram for the given detector """
 
     w1 = getType(det.type) # decrypted what(1)
-    title = "%s %s: reg %d %s %d #diamond %g cm^{2}" % (fluka.particle.get(det.dist, "undefined"), "fluence" if det.fluence else "current", det.reg1, "#leftrightarrow" if det.twoway else "#rightarrow", det.reg2, det.area)
+    title = "%s %s #diamond reg %d %s %d #diamond %g cm^{2}" % (fluka.particle.get(det.dist, "undefined"), "fluence" if det.fluence else "current", det.reg1, "#leftrightarrow" if det.twoway else "#rightarrow", det.reg2, det.area)
     title += getAxesTitle(det,w1[0])
     return ROOT.TH2F(det.name, title, det.ne, getEbins(det, w1[0]), det.na, getAbins(det, w1[0]))
 
