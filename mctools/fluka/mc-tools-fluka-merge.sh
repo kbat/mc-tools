0931978d88b8ce632f8861734b5ffa7037461fa2
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2019-07-03 19:25:19 +0200

    tests of args added

--- mctools/fluka/mc-tools-fluka-merge.sh
+++ mctools/fluka/mc-tools-fluka-merge.sh
@@ -5,8 +5,29 @@
 # with GNU parallel
 # The script assumes that the argv[1] extension
 # is the FLUKA merge tool name
+#
+# https://github.com/kbat/mc-tools
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 filename.ext"
+    exit 1
+fi
 
 in="$1"
-merge="${in##*.}"
+if [ ! -e ${in} ]; then
+    echo "File ${in} does not exist"
+    exit 2
+fi
 
-cat $in | $FLUTIL/$merge
+merge="${in##*.}"
+if [ ${merge} = ${in} ]; then
+    echo "${in} must have an extension"
+    exit 3
+fi
+
+if [ ! -e ${FLUTIL}/${merge} ]; then
+    echo "${FLUTIL}/${merge} does not exist"
+    exit 4
+fi
+
+cat ${in} | ${FLUTIL}/${merge}
