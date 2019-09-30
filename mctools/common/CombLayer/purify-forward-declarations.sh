039de7b0738686d0199e7adb2ad1f2e1eb91b501
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2019-09-30 14:43:00 +0200

    purify-forward-declarations.sh added

---
+++ mctools/common/CombLayer/purify-forward-declarations.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# Comments out unneded class forward declarations directives
+
+if [ ! $# -eq 2 ]; then
+    echo "Usage: $(basename $0) make-target /path/to/file.h"
+    exit 1
+fi
+
+target=$1
+fname=$2
+
+if [ ! -e "$fname" ]; then
+    echo "$fname does not exist"
+    exit 2
+fi
+
+if [ ! -e Makefile -a ! -e GNU‐makefile -a ! -e makefile ]; then
+    echo "Makefile does not exist"
+    exit 3
+fi
+
+tmp=$(mktemp -u)
+
+echo "Safe to remove in $fname:"
+grep "\ * class.*;" "$fname" | tac | while read -r line; do
+    /bin/cp -f "$fname" "$tmp"
+    sed -i -e "s/$line/\/\/ $line/" "$fname"
+    if make $target >& /dev/null; then
+	echo $line
+    else
+	/bin/cp -f $tmp $fname
+    fi
+done
+
+rm -f "$tmp"
