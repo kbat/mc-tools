63b8558c750220c020ed303979f591a9e80472ab
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2019-11-14 12:07:38 +0100

    h2ascii: separate functions for different objects

--- mctools/common/h2ascii.py
+++ mctools/common/h2ascii.py
@@ -6,52 +6,56 @@ import argparse
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
+def h12ascii(h):
+    """ Convert TH1 into text """
+    print("#", h.GetZaxis().GetTitle())
+    print("# xmin xmax val err")
+    nbins = h.GetNbinsX()
+    error = 0.0
+    for bin in range(1, nbins+1):
+        print(h.GetBinLowEdge(bin), h.GetBinLowEdge(bin+1), h.GetBinContent(bin), h.GetBinError(bin))
+
+def h22ascii(h):
+    """ Convert TH2 into text """
+    print("#", h.GetZaxis().GetTitle())
+    print("# xmin xmax ymin ymax val err")
+    nbinsx, nbinsy = h.GetNbinsX(), h.GetNbinsY()
+    for xbin in range(1, nbinsx+1):
+        for ybin in range(1, nbinsy+1):
+            x = h.GetXaxis()
+            y = h.GetYaxis()
+            print(x.GetBinLowEdge(xbin), x.GetBinLowEdge(xbin+1), y.GetBinLowEdge(ybin), y.GetBinLowEdge(ybin+1), h.GetBinContent(xbin, ybin), h.GetBinError(xbin, ybin))
+
+def g2ascii(g):
+    """ Convert TGraph and TGraphErrors into text """
+    print("# x errx y erry")
+    n = g.GetN()
+    for i in range(n):
+        print(g.GetX()[i], g.GetEX()[i], g.GetY()[i], g.GetEY()[i])
+
 def main():
     """ Converts TH[12] and TGraphErrors into ASCII format"""
 
     parser = argparse.ArgumentParser(description=main.__doc__,
                                     epilog="Homepage: https://github.com/kbat/mc-tools")
-    parser.add_argument('fname', type=str, help='ROOT file name')
-    parser.add_argument('hname', type=str, help='Object name')
+    parser.add_argument('fname',   type=str, help='ROOT file name')
+    parser.add_argument('objname', type=str, help='Object name')
 
     args = parser.parse_args()
 
-    fname = args.fname
-    hname = args.hname
+    f = ROOT.TFile(args.fname)
+    obj = f.Get(args.objname)
 
-    f = ROOT.TFile(fname)
-    h = f.Get(hname)
-    print("#", h.GetName(), h.GetTitle())
-    print("#", h.GetXaxis().GetTitle())
-    print("#", h.GetYaxis().GetTitle())
-    if h.InheritsFrom("TH1") or h.InheritsFrom("TH2"):
-        print("#", h.GetZaxis().GetTitle())
-        print("# xmin xmax y y_rel_error")
-    elif h.InheritsFrom("TGraph"):
-        print("# x x_abs_err y y_abs_err")
+    print("#", obj.GetName(), obj.GetTitle())
+    print("#", obj.GetXaxis().GetTitle())
+    print("#", obj.GetYaxis().GetTitle())
 
-    if h.InheritsFrom("TH2"):
-        nbinsx, nbinsy = h.GetNbinsX(), h.GetNbinsY()
-#        print(nbinsx, nbinsy)
-        for xbin in range(1, nbinsx+1):
-            for ybin in range(1, nbinsy+1):
-                error = 0.0
-                val = h.GetBinContent(xbin, ybin)
-                if val != 0:  error = h.GetBinError(xbin, ybin) / val
-                print(h.GetXaxis().GetBinLowEdge(xbin), h.GetXaxis().GetBinLowEdge(xbin+1), h.GetYaxis().GetBinLowEdge(ybin), h.GetYaxis().GetBinLowEdge(ybin+1), val, error)
-
-    elif h.InheritsFrom("TH1"):
-        nbins = h.GetNbinsX()
-        error = 0.0
-        for bin in range(1, nbins+1):
-            #        print(h.GetBinLowEdge(bin), h.GetBinLowEdge(bin+1), h.GetBinContent(bin), h.GetBinError(bin)/h.GetBinContent(bin))
-            error = 0.0
-            if h.GetBinContent(bin) != 0:  error = h.GetBinError(bin) / h.GetBinContent(bin)
-            print(h.GetBinLowEdge(bin), h.GetBinLowEdge(bin+1), h.GetBinContent(bin), error)
-    elif h.InheritsFrom("TGraph"):
-        n = h.GetN()
-        for i in range(n):
-            print(i, h.GetX()[i], h.GetEX()[i], h.GetY()[i], h.GetEY()[i])
+    if obj.InheritsFrom("TH2"):
+        h22ascii(obj)
+    elif obj.InheritsFrom("TH1"):
+        h12ascii(obj)
+    elif obj.InheritsFrom("TGraph"):
+        g2ascii(obj)
 
 if __name__ == "__main__":
     exit(main())
