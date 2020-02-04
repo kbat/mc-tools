b58bab4794253b34eeb980aff86ba4084aea1885
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2020-02-04 11:40:22 +0100

    fluka2root tests added

--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -1,9 +1,37 @@
 #! /bin/python
 
+import os
 import sys
+import shutil
+import tempfile
 
 def test_dummy():
         print("test_dummy")
         val = 1
         assert val == 1
 
+def test_root():
+        print("test root")
+        pass
+
+def fluka2root(inp):
+        tmpdir = tempfile.mkdtemp(suffix='.mc-tools')
+        inpfrom = os.path.join(os.environ["FLUPRO"], inp)
+        inpto = os.path.join(tmpdir, inp)
+        shutil.copyfile(inpfrom, inpto)
+
+        os.chdir(tmpdir)
+
+        cmd = "$FLUTIL/rfluka -N0 -M2 " + inp
+        val = os.system(cmd)
+        assert val == 0
+
+        cmd = "fluka2root " + inp
+        val = os.system(cmd)
+        assert val == 0
+
+        shutil.rmtree(tmpdir)
+
+def test_fluka2root():
+        for inp in ("example.inp", "exmixed.inp", "exdefi.inp", "exfixed.inp"):
+                fluka2root(inp)
