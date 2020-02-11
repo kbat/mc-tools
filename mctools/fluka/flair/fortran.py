6522a2b63df699da510ebafad09f1805a1949ddb
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2020-02-11 16:18:17 +0100

    flair files updated from the latest version

--- mctools/fluka/flair/fortran.py
+++ mctools/fluka/flair/fortran.py
@@ -1,38 +1,11 @@
 #!/bin/env python3
+# -*- coding: utf-8 -*-
 #
-# Copyright and User License
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Copyright Vasilis.Vlachoudis@cern.ch for the
-# European Organization for Nuclear Research (CERN)
+# Copyright European Organization for Nuclear Research (CERN)
+# All rights reserved
 #
-# Please consult the flair documentation for the license
-#
-# DISCLAIMER
-# ~~~~~~~~~~
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
-# NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY, OF
-# SATISFACTORY QUALITY, AND FITNESS FOR A PARTICULAR PURPOSE
-# OR USE ARE DISCLAIMED. THE COPYRIGHT HOLDERS AND THE
-# AUTHORS MAKE NO REPRESENTATION THAT THE SOFTWARE AND
-# MODIFICATIONS THEREOF, WILL NOT INFRINGE ANY PATENT,
-# COPYRIGHT, TRADE SECRET OR OTHER PROPRIETARY RIGHT.
-#
-# LIMITATION OF LIABILITY
-# ~~~~~~~~~~~~~~~~~~~~~~~
-# THE COPYRIGHT HOLDERS AND THE AUTHORS SHALL HAVE NO
-# LIABILITY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL,
-# CONSEQUENTIAL, EXEMPLARY, OR PUNITIVE DAMAGES OF ANY
-# CHARACTER INCLUDING, WITHOUT LIMITATION, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES, LOSS OF USE, DATA OR PROFITS,
-# OR BUSINESS INTERRUPTION, HOWEVER CAUSED AND ON ANY THEORY
-# OF CONTRACT, WARRANTY, TORT (INCLUDING NEGLIGENCE), PRODUCT
-# LIABILITY OR OTHERWISE, ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-# DAMAGES.
-#
-# Author:       Vasilis.Vlachoudis@cern.ch
-# Date: 24-Oct-2006
+# Author: Vasilis.Vlachoudis@cern.ch
+# Date:   24-Oct-2006
 
 __author__ = "Vasilis Vlachoudis"
 __email__  = "Vasilis.Vlachoudis@cern.ch"
@@ -45,14 +18,14 @@ import struct
 # @return size, None for EOF
 #-------------------------------------------------------------------------------
 def skip(f):
-        blen = f.read(4)
-        if len(blen)==0: return 0
-        (size,) = struct.unpack("=i", blen)
-        f.seek(size, 1)
-        blen2 = f.read(4)
-        if blen != blen2:
-                raise IOError("Skipping fortran block")
-        return size
+	blen = f.read(4)
+	if not blen: return 0
+	(size,) = struct.unpack("=i", blen)
+	f.seek(size, 1)
+	blen2 = f.read(4)
+	if blen != blen2:
+		raise IOError("Skipping fortran block")
+	return size
 
 #-------------------------------------------------------------------------------
 # Read a fortran structure from a binary file
@@ -60,22 +33,22 @@ def skip(f):
 # @return data, None for EOF
 #-------------------------------------------------------------------------------
 def read(f):
-        blen = f.read(4)
-        if len(blen)==0: return None
-        (size,) = struct.unpack("=i", blen)
-        data  = f.read(size)
-        blen2 = f.read(4)
-        if blen != blen2:
-                raise IOError("Reading fortran block")
-        return data
+	blen = f.read(4)
+	if not blen: return None
+	(size,) = struct.unpack("=i", blen)
+	data  = f.read(size)
+	blen2 = f.read(4)
+	if blen != blen2:
+		raise IOError("Reading fortran block")
+	return data
 
 #-------------------------------------------------------------------------------
 # Write a block of data (string) to file as a fortran block
-# @param f      file to write to
-# @param d      data to write
+# @param f	file to write to
+# @param d	data to write
 # @return output of last write statement
 #-------------------------------------------------------------------------------
 def write(f, d):
-        f.write(struct.pack("=i",len(d)))
-        f.write(d)
-        return f.write(struct.pack("=i",len(d)))
+	f.write(struct.pack("=i",len(d)))
+	f.write(d)
+	return f.write(struct.pack("=i",len(d)))
