ac6ff0235686e83f84cfbdac104efc4b46517e5a
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2020-03-30 17:04:29 +0200

    bugfix: zmax treatment when plotting errors

--- mctools/common/plot2d.py
+++ mctools/common/plot2d.py
@@ -140,7 +140,7 @@ def main():
                 if args.zmin:
                         args.zmin = 0
                         if args.verbose: print("zmin set to 0")
-                if args.zmax>100:
+                if args.zmax and args.zmax>100:
                         args.zmax = 100
                         if args.verbose: print("zmax set to 100")
                 args.logz = False
