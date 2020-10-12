b1e9a0583ea1b81a726cf76ae868d27038011ff7
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2020-10-12 12:36:01 +0200

    RebinToScreen function added

--- mctools/common/plot2d/src/plot2d.cxx
+++ mctools/common/plot2d/src/plot2d.cxx
@@ -10,10 +10,40 @@
 #include <TH3F.h>
 #include <TH1.h>
 #include <TObjectTable.h>
+#include <TMath.h>
+#include <TVirtualX.h>
 //#include <TRint.h>
 #include "Arguments.h"
 #include "MainFrame.h"
 #include "Data.h"
+
+void RebinToScreen(std::shared_ptr<TH2F> h2)
+{
+  std::clog << "RebinToScreen" << std::endl;
+
+  const Int_t nx = h2->GetNbinsX();
+  const Int_t ny = h2->GetNbinsY();
+
+  Int_t x, y;
+  UInt_t w, h;
+  gVirtualX->GetWindowSize(gClient->GetRoot()->GetId(), x, y, w, h);
+  //  std::cout << x << " " << y << std::endl;
+  std::cout << " screen size: " << w << " " << h << std::endl;
+
+  std::cout << " h2 before: " << h2->GetNbinsX() << " " << h2->GetNbinsY() << std::endl;
+
+  const Int_t scaleX =
+    TMath::Ceil(h2->GetNbinsX()/static_cast<float>(w));
+  if (scaleX>=2)
+    h2->RebinX(scaleX);
+  const Int_t scaleY =
+    TMath::Ceil(h2->GetNbinsY()/static_cast<float>(h));
+  if (scaleY>=2)
+    h2->RebinY(scaleY);
+
+  std::cout << "h2 after: " << h2->GetNbinsX() << " " << h2->GetNbinsY() << std::endl;
+  return;
+}
 
 void SetColourMap()
 {
@@ -65,52 +95,37 @@ int main(int argc, const char **argv)
   //  gROOT->SetBatch(true);
   gStyle->SetOptStat(kFALSE);
 
-  // const size_t width = vm["width"].as<size_t>();
-  // size_t height = vm["height"].as<size_t>();
-  // if (height==0)
-  //   height = int(width*2.0/(1.0+sqrt(5.0))); // golden ratio
-
   std::string dfname = vm["dfile"].as<std::string>();
-  // TFile *df = new TFile(dfname.c_str());
-  // if (df->IsZombie())
-  //   return 1;
-  // df->ls();
 
   std::string dhname = vm["dhist"].as<std::string>();
-  Data data(dfname, dhname, args.GetPlane());
+  Data data(dfname, dhname, &args);
+
   const std::shared_ptr<TH3F> h3 = data.GetH3();
-  const std::shared_ptr<TH2F> h2a = data.GetH2();
+  const std::shared_ptr<TH2F> h2 = data.GetH2();
+
+  std::cout << "h2 bins: " << h2->GetNbinsX() << " " << h2->GetNbinsY() << std::endl;
 
   TApplication theApp("App",&argc,const_cast<char**>(argv));
 
-
-  MainFrame *mf = new MainFrame(gClient->GetRoot(), width, height);
-  mf->SetWindowName(args.GetTitle().c_str());
+  MainFrame *mf = new MainFrame(gClient->GetRoot(), args.GetWidth(), args.GetHeight());
+  mf->SetWindowName(args.GetWindowTitle().c_str());
   SetColourMap();
 
-  // TCanvas *c1 = mf->GetCanvas();
-  // if (args.IsSlice())
-  //   c1->Divide(1,2);
 
-  // c1->cd(1);
-  // int nthreads = 4;
-  // ROOT::EnableImplicitMT(nthreads);
+  TCanvas *c1 = mf->GetCanvas();
+  if (args.GetZTitle() != "None")
+    c1->SetRightMargin(args.GetMap()["right_margin"].as<float>());
 
-  // auto start = std::chrono::high_resolution_clock::now();
-  // TH1 *h2 = h3->Project3D(args.GetPlane().c_str());
-  // auto delta = std::chrono::high_resolution_clock::now()-start;
-  // std::cout << " Project3D (ms) " << std::chrono::duration_cast<std::chrono::milliseconds>(delta).count() << std::endl;
 
-  // h2->Draw("colz"); // 3 sec to draw
+  RebinToScreen(h2);
 
-  // c1->cd(2);
-  // h2a->Draw("colz"); // 3 sec to draw
+  h2->Draw("colz"); // 3 sec to draw
 
   //    gObjectTable->Print();
 
   theApp.Run();
 
-  // //  delete mf;
+  //  delete mf;
 
 
   return 0;
@@ -119,5 +134,5 @@ int main(int argc, const char **argv)
 // Performance:
 // h2  - Project3D + draw = 8 sec (same time with Python script)
 //   since it takes 8 sec to draw -> 5 sec to Project3D
-// h2a - project + draw = 5 sec (since it takes 3 sec to draw -> 2 sec to project)
+// h2 - Data::Project + draw = 5 sec (since it takes 3 sec to draw -> 2 sec to project)
 // project and draw both = 13 sec -> correct
