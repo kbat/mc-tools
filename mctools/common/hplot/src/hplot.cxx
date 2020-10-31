71dfcf4ce85bf12f2f393f4f5b28a2a336513597
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2020-10-31 09:20:00 +0100

    a bug fixed in SetColourMap

--- mctools/common/hplot/src/hplot.cxx
+++ mctools/common/hplot/src/hplot.cxx
@@ -52,13 +52,13 @@ void SetColourMap()
   // PView is exported from ParaView
   const std::vector<Float_t>
     PView{0, 0.27843137254900002, 0.27843137254900002, 0.85882352941200002,
-	  0.143, 0, 0, 0.36078431372500003,
-	  0.285, 0, 1, 1,
-	  0.429, 0, 0.50196078431400004, 0,
-	  0.571, 1, 1, 0,
-	  0.714, 1, 0.38039215686299999, 0,
-	  0.857, 0.419607843137, 0, 0,
-	  1, 0.87843137254899994, 0.30196078431399997, 0.30196078431399997};
+  	  0.143, 0, 0, 0.36078431372500003,
+  	  0.285, 0, 1, 1,
+  	  0.429, 0, 0.50196078431400004, 0,
+  	  0.571, 1, 1, 0,
+  	  0.714, 1, 0.38039215686299999, 0,
+  	  0.857, 0.419607843137, 0, 0,
+  	  1, 0.87843137254899994, 0.30196078431399997, 0.30196078431399997};
 
   constexpr UInt_t NColors = 99;
   constexpr UInt_t NRGBs = 8;
@@ -67,12 +67,12 @@ void SetColourMap()
 
   for (UInt_t i=0; i<NRGBs; ++i) {
     stops.push_back(PView[4*i]);
-    red.push_back(PView[4*i]+1);
-    green.push_back(PView[4*i]+2);
-    blue.push_back(PView[4*i]+3);
+    red.push_back(PView[4*i+1]);
+    green.push_back(PView[4*i+2]);
+    blue.push_back(PView[4*i+3]);
   }
 
-  TColor::CreateGradientColorTable(NRGBs, &stops[0], &red[0], &green[0], &blue[0], NColors);
+  TColor::CreateGradientColorTable(NRGBs, stops.data(), red.data(), green.data(), blue.data(), NColors);
   gStyle->SetNumberContours(NColors);
 
   return;
@@ -92,7 +92,6 @@ int main(int argc, const char **argv)
   const po::variables_map vm = args.GetMap();
 
   gStyle->SetOptStat(kFALSE);
-  gStyle->SetPalette(kTemperatureMap);
   SetColourMap();
 
   std::string dfname = vm["dfile"].as<std::string>();
