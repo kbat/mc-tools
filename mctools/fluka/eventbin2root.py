f72a6fb637147192a756af052a4481cc4a34e821
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2024-08-12 14:00:15 +0200

    first header is read

---
+++ mctools/fluka/eventbin2root.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import sys, argparse, os, struct
+from array import array
+from mctools.fluka.flair import fortran
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+
+def main():
+    """ Converts EVENTBIN output into a ROOT TTree """
+
+    parser = argparse.ArgumentParser(description=main.__doc__,
+                                     epilog="Homepage: https://github.com/kbat/mc-tools")
+    parser.add_argument('eventbin', type=str, nargs='*', help='list of EVENTBIN files')
+    parser.add_argument('-o', dest='root', type=str, help='output ROOT file name', default="")
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, dest='verbose', help='print what is being done')
+    parser.add_argument('-f', '--force', action='store_true', default=False, dest='overwrite', help='overwrite the output ROOT file')
+
+
+    args = parser.parse_args()
+
+    for f in args.eventbin:
+        if not os.path.isfile(f):
+            print("eventbin2root: File %s does not exist." % f, file=sys.stderr)
+            return 1
+
+    if not args.overwrite and os.path.isfile(args.root):
+        sys.exit("%s exists. Use '-f' to overwrite it." % args.root)
+
+    first = True
+    for eventbin in args.eventbin:
+        with open(eventbin, 'rb') as f:
+            print(eventbin)
+            data = fortran.read(f)
+            title, time = struct.unpack("=80s32s", data)
+            title = title.decode('utf8').strip()
+            time = time.decode('utf8').strip()
+            print(time)
+
+            while True:
+                data = fortran.read(f)
+                size = len(data)
+                print("size:",size)
+                if data is None:
+                    break
+
+                if first:
+                    first = False
+
+                mb,titusb,itusbn,idusbn, \
+                    xlow,xhigh,nxbin,dxusbn, \
+                    ylow,yhigh,nybin,dyusbn, \
+                    zlow,zhigh,nzbin,dzusbn, \
+                    lntzer,bkusbn,b2usbn,tcusbn,tmp = struct.unpack("=i10s2i2fi3fi3fifi3fi",data)
+                titusb = titusb.decode('utf8').strip() # estimator title
+
+                print(mb,titusb,itusbn,idusbn,xlow,xhigh,nxbin,dxusbn,ylow,yhigh,nybin,dyusbn,zlow,zhigh,nzbin,dzusbn,lntzer,bkusbn,b2usbn,tcusbn,tmp)
+
+                data = fortran.read(f)
+                size = len(data)
+                print("size:",size)
+                a,b,c,d = struct.unpack("=iifi",data)
+                print(a,b,c,d)
+
+                data = fortran.read(f)
+                size = len(data)
+                print("size:",size)
+
+
+                break
+
+#                    title, time, nregs, nsco, dist = struct.unpack("=80s32siii", data)
+
+            #         print("title:", title)
+            #         print("time:", time)
+            #         print("number of regions:",nregs)
+            #         print("number of scoring distributions:", nsco)
+            #         print("distribution:",dist)
+
+            #         DATA = array('f', nsco*nregs*[0.0])
+
+            #         fout = ROOT.TFile(args.root, "recreate", title)
+            #         T = ROOT.TTree("EVENTBIN", time)
+            #         T.Branch("DATA", DATA, "d%d[%d]/F" % (dist,nsco*nregs))
+            #     elif size == 12:
+            #         pass
+            #     elif size == 48:
+            #         pass
+            #     elif size == 8:
+            #         pass
+            #     elif size == nregs*nsco*4:
+            #         val = struct.unpack("%df" % nregs*nsco, data)
+            #         for i,v in enumerate(val):
+            #             DATA[i] = v
+            #         T.Fill()
+
+    # T.Write()
+    # fout.Close()
+
+
+if __name__=="__main__":
+    sys.exit(main())
