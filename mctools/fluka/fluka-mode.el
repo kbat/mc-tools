ae21bd28b318f4c285ba62e3b330f0b71b0c6362
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2024-09-29 07:17:14 +0200

    fluka-mode: WHAT separators added

--- mctools/fluka/fluka-mode.el
+++ mctools/fluka/fluka-mode.el
@@ -152,16 +152,6 @@
           ;; in general, put longer words first
           )))
 
-;;;###autoload
-(define-derived-mode fluka-mode fundamental-mode "FLUKA mode"
-  "Major mode for editing FLUKA input files"
-
-  ;; code for syntax highlighting
-  (setq font-lock-defaults '((fluka-font-lock-keywords))))
-
-;; add the mode to the `features' list
-(provide 'fluka-mode)
-
 ;; test a tool tip - does not work
 ;;(insert (propertize "foo\n" 'help-echo "Tooltip!"))
 
@@ -177,5 +167,132 @@
 			  )
     )
   )
+
+
+;;; Vertical lines to separate WHATs
+;;; Currently, a copy of https://www.emacswiki.org/emacs/column-marker.el
+;;; to avoid depending on additional external file, but the intention is to simplify it
+;;; as much as possible and better adapt for the FLUKA syntax
+;;; (for example, avoid separators in the comment lines).
+
+(defface column-marker-1 '((t (:background "#252525"))) ; my background is #303030
+  "Face used for WHAT separators.  Usually close to the background color."
+  :group 'faces)
+
+(defvar column-marker-1-face 'column-marker-1
+    "Face used for WHAT separators. Changing this directly affects only new separators.")
+
+(defvar column-marker-vars ()
+  "List of all internal column-marker variables")
+(make-variable-buffer-local 'column-marker-vars) ; Buffer local in all buffers.
+
+(defmacro column-marker-create (var &optional face)
+  "Define a column marker named VAR.
+FACE is the face to use.  If nil, then face `column-marker-1' is used."
+  (setq face (or face 'column-marker-1))
+  `(progn
+     ;; define context variable ,VAR so marker can be removed if desired
+     (defvar ,var ()
+       "Buffer local. Used internally to store column marker spec.")
+     ;; context must be buffer local since font-lock is
+     (make-variable-buffer-local ',var)
+     ;; Define wrapper function named ,VAR to call `column-marker-internal'
+     (defun ,var (arg)
+       ,(concat "Highlight column with face `" (symbol-name face)
+                "'.\nWith no prefix argument, highlight current column.\n"
+                "With non-negative numeric prefix arg, highlight that column number.\n"
+                "With plain `C-u' (no number), turn off this column marker.\n"
+                "With `C-u C-u' or negative prefix arg, turn off all column-marker highlighting.")
+       (interactive "P")
+       (unless (memq ',var column-marker-vars) (push ',var column-marker-vars))
+       (cond ((null arg)          ; Default: highlight current column.
+              (column-marker-internal ',var (1+ (current-column)) ,face))
+             ((consp arg)
+              (if (= 4 (car arg))
+                  (column-marker-internal ',var nil) ; `C-u': Remove this column highlighting.
+                (dolist (var column-marker-vars)
+                  (column-marker-internal var nil)))) ; `C-u C-u': Remove all column highlighting.
+             ((and (integerp arg) (>= arg 0)) ; `C-u 70': Highlight that column.
+              (column-marker-internal ',var (1+ (prefix-numeric-value arg)) ,face))
+             (t           ; `C-u -40': Remove all column highlighting.
+              (dolist (var column-marker-vars)
+                (column-marker-internal var nil)))))))
+
+(defun column-marker-find (col)
+  "Defines a function to locate a character in column COL.
+Returns the function symbol, named `column-marker-move-to-COL'."
+  (let ((fn-symb  (intern (format "column-marker-move-to-%d" col))))
+    (fset `,fn-symb
+          `(lambda (end)
+             (let ((start (point)))
+               (when (> end (point-max)) (setq end (point-max)))
+
+               ;; Try to keep `move-to-column' from going backward, though it still can.
+               (unless (< (current-column) ,col) (forward-line 1))
+
+               ;; Again, don't go backward.  Try to move to correct column.
+               (when (< (current-column) ,col) (move-to-column ,col))
+
+               ;; If not at target column, try to move to it.
+               (while (and (< (current-column) ,col) (< (point) end)
+                           (= 0 (+ (forward-line 1) (current-column)))) ; Should be bol.
+                 (move-to-column ,col))
+
+               ;; If at target column, not past end, and not prior to start,
+               ;; then set match data and return t.  Otherwise go to start
+               ;; and return nil.
+               (if (and (= ,col (current-column)) (<= (point) end) (> (point) start))
+                   (progn (set-match-data (list (1- (point)) (point)))
+                          t)            ; Return t.
+                 (goto-char start)
+                 nil))))                ; Return nil.
+    fn-symb))
+
+(defun column-marker-internal (sym col &optional face)
+  "SYM is the symbol for holding the column marker context.
+COL is the column in which a marker should be set.
+Supplying nil or 0 for COL turns off the marker.
+FACE is the face to use.  If nil, then face `column-marker-1' is used."
+  (setq face (or face 'column-marker-1))
+  (when (symbol-value sym)   ; Remove any previously set column marker
+    (font-lock-remove-keywords nil (symbol-value sym))
+    (set sym nil))
+  (when (or (listp col) (< col 0)) (setq col nil)) ; Allow nonsense stuff to turn off the marker
+  (when col                             ; Generate a new column marker
+    (set sym `((,(column-marker-find col) (0 ',face prepend t))))
+    (font-lock-add-keywords nil (symbol-value sym) t))
+  (font-lock-fontify-buffer))
+
+;; If you need more markers you can create your own similarly.
+;; All markers can be in use at once, and each is buffer-local,
+;; so there is no good reason to define more unless you need more
+;; markers in a single buffer.
+(column-marker-create column-marker-1 column-marker-1-face)
+(column-marker-create column-marker-2 column-marker-1-face)
+(column-marker-create column-marker-3 column-marker-1-face)
+(column-marker-create column-marker-4 column-marker-1-face)
+(column-marker-create column-marker-5 column-marker-1-face)
+(column-marker-create column-marker-6 column-marker-1-face)
+(column-marker-create column-marker-7 column-marker-1-face)
+
+;;(require 'column-marker)
+(add-hook 'fluka-mode-hook (lambda () (interactive) (column-marker-1 10)))
+(add-hook 'fluka-mode-hook (lambda () (interactive) (column-marker-2 20)))
+(add-hook 'fluka-mode-hook (lambda () (interactive) (column-marker-3 30)))
+(add-hook 'fluka-mode-hook (lambda () (interactive) (column-marker-4 40)))
+(add-hook 'fluka-mode-hook (lambda () (interactive) (column-marker-5 50)))
+(add-hook 'fluka-mode-hook (lambda () (interactive) (column-marker-6 60)))
+(add-hook 'fluka-mode-hook (lambda () (interactive) (column-marker-7 70)))
+
+
+;;;###autoload
+(define-derived-mode fluka-mode fundamental-mode "FLUKA mode"
+  "Major mode for editing FLUKA input files"
+
+  ;; code for syntax highlighting
+  (setq font-lock-defaults '((fluka-font-lock-keywords))))
+
+;; add the mode to the `features' list
+(provide 'fluka-mode)
 
 ;;; fluka-mode.el ends here
