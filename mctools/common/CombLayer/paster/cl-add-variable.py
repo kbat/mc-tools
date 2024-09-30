b7cba009a691a8c03b85c14ca36659fd5f86aeec
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2024-09-30 06:58:35 +0200

    indent updated

--- mctools/common/CombLayer/paster/cl-add-variable.py
+++ mctools/common/CombLayer/paster/cl-add-variable.py
@@ -158,7 +158,7 @@ def main():
     parser.add_argument('-class', dest='className', type=str, help='class name', required=True)
     parser.add_argument('-value', dest='value', type=str,default="", required=False,
                         help='default value (if set, the generator for variables is created and this value is set as default)')
-    parser.add_argument('-indent', dest='indent', type=int, help='number of initial spaces', default=2, required=False)
+    parser.add_argument('-indent', dest='indent', type=int, help='number of initial spaces', default=4, required=False)
     parser.add_argument('-generator', dest='generator', type=str, help='path to file with generator implementation (.cxx)', default="", required=False)
 
     args = parser.parse_args()
