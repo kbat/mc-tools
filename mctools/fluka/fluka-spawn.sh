a4bc86a91cf692dc502d1e838d7c20b7d627760f
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2024-10-25 17:26:18 +0200

    fluka-spawn sets initial random number seed

--- mctools/fluka/fluka-spawn.sh
+++ mctools/fluka/fluka-spawn.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-INP=$1
-NJOBS=$2
-
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
     echo "FLUKA input file spawner"
     echo "      generates clones of the given input file with different random seeds"
     echo ""
     echo "Usage: $(basename $0) INP njobs"
     echo " INP   FLUKA input file"
     echo " jobs  Number of input files with different random seeds to generate"
+    echo " seed  Random number seed offset. Will be incremented by 1 in each input file."
     echo ""
     echo "Homepage: https://github.com/kbat/mc-tools"
     exit 1
 fi
+
+INP=$1
+NJOBS=$2
+SEED=$3
 
 if [ ! -e "$INP" ]; then
     echo "Can't open $INP"
@@ -27,5 +29,6 @@ fi
 
 for (( j=1; j<=$NJOBS; j++ )); do
     NEWINP=$(basename $INP .inp)-spawn$j.inp
-   cat "$INP" | sed "s;^RANDOMI.*;RANDOMIZE        1.0 $j.0;" > $NEWINP
+    R=$((SEED + j))
+   cat "$INP" | sed "s;^RANDOMI.*;RANDOMIZE        1.0 $R.0;" > $NEWINP
 done
